5f5b8a40fb70b2e9d653beae3da8ffecc5cb68e1
Gargi <gauridilip2001@gmail.com>
2025-09-13 22:51:01 +0530

    Update script.R

--- script.R
+++ script.R
@@ -12,17 +12,13 @@ abline(v=c(-1,1), col="blue", lty=2)     # Log2FC cutoff
 abline(h=-log10(0.01), col="red", lty=2) # p-value cutoff
 
 
-
 ##Identify Up-regulated Genes
 upregulated <- subset(data, log2FoldChange > 1 & pvalue < 0.01)
 head(upregulated)
 
-
-##
 ##Identify down regulated Genes
 downregulated <- subset(data, log2FoldChange < -1 & pvalue < 0.01)
 head(downregulated)
-
 
 
 # Sort by p-value (or abs(log2FC))
@@ -31,5 +27,6 @@ top5_down <- head(downregulated[order(downregulated$pvalue), ], 5)
 
 top5_up$Gene
 top5_down$Gene
+
 
 
